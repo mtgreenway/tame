5248408c981cff0261d7684164c7663d640a36dc
Allison Heath <aheath@uchicago.edu>
2013-09-18 14:53:20 -0500

    Uncommenting commented lines

--- tcga/setup/post_install_couchdb.sh
+++ tcga/setup/post_install_couchdb.sh
@@ -1,5 +1,5 @@
 # change file ownership from root to couchdb user and adjust permissions
-#useradd -d /usr/local/var/lib/couchdb couchdb
+useradd -d /usr/local/var/lib/couchdb couchdb
 chown -R couchdb: /usr/local/var/lib/couchdb /usr/local/var/log/couchdb /usr/local/var/run/couchdb /usr/local/etc/couchdb /usr/local/lib/couchdb /usr/local/shar
 e/couchdb
 chmod 0770 /usr/local/var/lib/couchdb /usr/local/var/log/couchdb /usr/local/var/run/couchdb /usr/local/lib/couchdb /usr/local/share/couchdb
@@ -7,11 +7,11 @@ chmod 664 /usr/local/etc/couchdb/*.ini
 chmod 775 /usr/local/etc/couchdb/*.d
 
 # start couchdb
-#cd /etc/init.d
-#ln -s /usr/local/etc/init.d/couchdb couchdb
-#/etc/init.d/couchdb start
+cd /etc/init.d
+ln -s /usr/local/etc/init.d/couchdb couchdb
+/etc/init.d/couchdb start
 # Start couchdb on system start
-#update-rc.d couchdb defaults
+update-rc.d couchdb defaults
 
 # Verify couchdb is running
-#curl http://127.0.0.1:5984/
+curl http://127.0.0.1:5984/
